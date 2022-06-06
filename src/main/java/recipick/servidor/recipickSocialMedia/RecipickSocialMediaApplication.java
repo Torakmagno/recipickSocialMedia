38e851d63aed94ed663c830b1e25a9d630a87d0a
TorakMagno <91882681+Torakmagno@users.noreply.github.com>
2022-06-06 19:18:53 +0200

    acabando

--- src/main/java/recipick/servidor/recipickSocialMedia/RecipickSocialMediaApplication.java
+++ src/main/java/recipick/servidor/recipickSocialMedia/RecipickSocialMediaApplication.java
@@ -1,7 +1,11 @@
 package recipick.servidor.recipickSocialMedia;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class RecipickSocialMediaApplication {
